5eb73ea07cd5409baf2e90343fa305b24632c722
Marisa <marisatriguerosperez@gmail.com>
2024-06-18 00:41:48 +0200

    Ejercicio 1

---
+++ Basic-concepts-project/src/components/Age.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Age({ age }) {
+  return (
+    <div>Your age is { age }</div>
+  )
+}
+
+export default Age
---
+++ Basic-concepts-project/src/components/Hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Message from './Message'
+
+function Hello() {
+  return (
+    <>
+        <h2>Hello, World!</h2>
+        <Message />
+    </>
+  )
+}
+
+export default Hello
