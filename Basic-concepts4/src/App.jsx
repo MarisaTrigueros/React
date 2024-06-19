d907501389722446bd685c0ec142d70cc01f6ddf
Marisa <marisatriguerosperez@gmail.com>
2024-06-20 00:26:34 +0200

    Counter

---
+++ Basic-concepts4/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react';
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <>
+     
+     <Counter initialValue={0} click={1}/>
+        
+    </>
+  )
+}
+
+export default App
